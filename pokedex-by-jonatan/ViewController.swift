f866882a1358ac37d3aa9ade38356e8ac4fbd88e
Jonatan Knudsen <jonatanknudsen@jonatans-macbook-pro.local>
2016-01-07 16:34:33 +0100

    Initial Commit

---
+++ pokedex-by-jonatan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-jonatan
+//
+//  Created by Jonatan Knudsen on 07/01/2016.
+//  Copyright © 2016 Jonatan Knudsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
