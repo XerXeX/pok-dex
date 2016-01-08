125cf2aaef9bdfe8020bf1baa938e8e47dea48b4
Jonatan Knudsen <jonatanknudsen@jonatans-macbook-pro.local>
2016-01-08 12:09:46 +0100

    App almost done

---
+++ pokedex-by-jonatan/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex-by-jonatan
+//
+//  Created by Jonatan Knudsen on 08/01/2016.
+//  Copyright © 2016 Jonatan Knudsen. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
